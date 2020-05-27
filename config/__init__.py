761e87c3b81fee548f91e468c3dc2eb5bf89a569
wbc <salivawbc@gmail.com>
2020-05-27 23:07:23 +0800

    change env

---
+++ config/__init__.py
@@ -0,0 +1,4 @@
+JD_SHOP_FETCH_HOST = "shop.m.jd.com"
+JD_ENTERPRISE_FETCH_HOST = "shopjump.m.jd.com"
+JD_SHOP_TEMPLATE_URL = "https://%s/index/getShopTemplate.json?shopId={shop_id}" % JD_SHOP_FETCH_HOST
+JD_ENTERPRISE_LICENSE_URL = "https://%s/shopdetail/shoplicense?shopId={shop_id}" % JD_ENTERPRISE_FETCH_HOST
